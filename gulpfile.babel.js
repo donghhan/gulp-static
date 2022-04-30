b84b4a686930378abdbea4f689659eddfb9fb5ed
Donghoon Han <apensia@Donghoons-MacBook-Pro.local>
2022-04-30 11:05:49 +0900

    Configuration of webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
-import { src, dest, series } from "gulp";
+import { src, dest, series, watch, parallel } from "gulp";
 import pug from "gulp-pug";
+import del from "del";
+import webserver from "gulp-webserver";
 
 const routes = {
   pug: {
@@ -8,9 +10,27 @@ const routes = {
   },
 };
 
+// Deleting all old build files
+function Clean() {
+  return del(["build/"]);
+}
+
+// Live server
+function Webserver() {
+  return src("build").pipe(webserver({ livereload: true, port: 4500 }));
+}
+
 // Compiling Pug into HTML
 function PugIntoHTML() {
   return src(routes.pug.src).pipe(pug()).pipe(dest(routes.pug.build));
 }
 
-export const dev = series([PugIntoHTML]);
+function Watch() {
+  watch(routes.pug.src, PugIntoHTML);
+}
+
+const pre = series([Clean]);
+const build = series([PugIntoHTML]);
+const post = parallel([Webserver, Watch]);
+
+export const dev = series(pre, build, post);
